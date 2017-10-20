cb7d0e81c1c94dbdb886c8e2e58a53fbf88e8699
Wellington Guimaraes <wellguimaraes@gmail.com>
2017-10-20 03:31:54 -0300

    updated dependencies

--- tests/matchSnapshot.test.js
+++ tests/matchSnapshot.test.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { expect } from 'chai';
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme, { shallow } from 'enzyme'
+import { expect } from 'chai'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 function MyReactComponent() {
   return (
     <div className="abc-234-45">
       Lorem Ipsum dolor
     </div>
-  );
+  )
 }
 
 describe('matchSnapshot', () => {
   it('should match snapshots', () => {
-    const wrapper = shallow(<MyReactComponent />);
+    const wrapper = shallow(<MyReactComponent/>)
 
-    expect(wrapper).to.matchSnapshot();
-    expect(123).to.matchSnapshot();
-    expect({ a: 1, b: { c: 'lorem' } }).to.matchSnapshot();
-  });
-});
+    expect(wrapper).to.matchSnapshot()
+    expect(123).to.matchSnapshot()
+    expect({ a: 1, b: { c: 'lorem' } }).to.matchSnapshot()
+  })
+})
