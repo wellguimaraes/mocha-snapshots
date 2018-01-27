2c3d25a12604af4c9c4aeec0f252425982ec9006
Wellington Guimaraes <wellguimaraes@gmail.com>
2018-01-26 22:56:36 -0600

    stop rendering null props on snapshots

--- src/stringify.js
+++ src/stringify.js
@@ -1,3 +1,8 @@
+function ignoreNulls(key,value) {
+  if (value === null) return undefined
+  return value
+}
+
 module.exports = function stringify(obj) {
-  return JSON.stringify(obj, null, '  ');
+  return JSON.stringify(obj, ignoreNulls, '  ');
 };
