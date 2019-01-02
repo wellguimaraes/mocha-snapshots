cb4d4f00ee8a761461f8ccefba19dfdfa44c20a5
Wellington Guimaraes <wellguimaraes@gmail.com>
2019-01-02 16:01:57 -0300

    new version with fixed test path resolving

--- src/getTestName.js
+++ src/getTestName.js
@@ -1,11 +1,13 @@
 module.exports = function getTestName(context) {
   let ctxPath = [context.title];
   let stepParent = context.runnable.parent;
+
   while (stepParent) {
     if (stepParent.title) {
       ctxPath.unshift(stepParent.title);
     }
     stepParent = stepParent.parent;
   }
+
   return ctxPath.join('/').trim();
 };
